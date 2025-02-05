cea894579a6ebcce7f04e64575340012eccf811e
John Patrick Buco <patrick.buco@lamudi.com>
2025-02-05 21:26:35 +0800

    feat: dashboard API to view order statistics

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Order;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,8 +13,31 @@ class AdminController extends AbstractController
 {
     #[Route('/api/admin/dashboard', name: 'admin_dashboard', methods: ['GET'])]
     #[IsGranted('ROLE_ADMIN')]
-    public function dashboard(): JsonResponse
+    public function getOrderStatistics(EntityManagerInterface $entityManager): JsonResponse
     {
-        return new JsonResponse(['message' => 'Welcome, Admin!']);
+        $orderRepo = $entityManager->getRepository(Order::class);
+
+        $totalOrders = $orderRepo->count([]);
+        $totalRevenue = $orderRepo->createQueryBuilder('o')
+            ->select('SUM(o.total)')
+            ->getQuery()
+            ->getSingleScalarResult();
+
+        $statusCounts = $orderRepo->createQueryBuilder('o')
+            ->select('o.status, COUNT(o.id) as count')
+            ->groupBy('o.status')
+            ->getQuery()
+            ->getResult();
+
+        $statusData = [];
+        foreach ($statusCounts as $status) {
+            $statusData[$status['status']->value] = (int) $status['count'];
+        }
+
+        return new JsonResponse([
+            'total_orders' => $totalOrders,
+            'total_revenue' => (float) $totalRevenue,
+            'orders_by_status' => $statusData
+        ]);
     }
 }
