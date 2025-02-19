55e4f3f51c0b9a768eb0c9eb035f1cce8494d4ed
John Patrick Buco <patrick.buco@lamudi.com>
2025-02-19 08:27:48 +0800

    refactor: add auth service

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -3,6 +3,7 @@
 namespace Bocum\Controller;
 
 use Bocum\Entity\User;
+use Bocum\Service\AuthService;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -13,50 +14,19 @@ use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class RegisterController extends AbstractController
 {
+    private AuthService $authService;
+
+    public function __construct(AuthService $authService)
+    {
+        $this->authService = $authService;
+    }
+
     #[Route('/api/register', name: 'api_register', methods: ['POST'])]
-    public function register(
-        Request $request,
-        EntityManagerInterface $entityManager,
-        UserPasswordHasherInterface $passwordHasher,
-        ValidatorInterface $validator
-    ): JsonResponse {
+    public function register(Request $request): JsonResponse
+    {
         $data = json_decode($request->getContent(), true);
+        $result = $this->authService->registerUser($data);
 
-        // Validate required fields
-        if (!isset($data['email'], $data['password'])) {
-            return new JsonResponse(['error' => 'Email and password are required'], JsonResponse::HTTP_BAD_REQUEST);
-        }
-
-        // Check if user already exists
-        $existingUser = $entityManager->getRepository(User::class)->findOneBy(['email' => $data['email']]);
-        if ($existingUser) {
-            return new JsonResponse(['error' => 'User already exists'], JsonResponse::HTTP_CONFLICT);
-        }
-
-        // Create new user
-        $user = new User();
-        $user->setEmail($data['email']);
-        $hashedPassword = $passwordHasher->hashPassword($user, $data['password']);
-        $user->setPassword($hashedPassword);
-
-        // Allow assigning ROLE_ADMIN, but only if explicitly provided
-        $roles = $data['roles'] ?? ['ROLE_USER'];
-        $user->setRoles($roles);
-
-        // Validate the User entity
-        $errors = $validator->validate($user);
-        if (count($errors) > 0) {
-            $errorMessages = [];
-            foreach ($errors as $error) {
-                $errorMessages[] = $error->getMessage();
-            }
-            return new JsonResponse(['errors' => $errorMessages], JsonResponse::HTTP_BAD_REQUEST);
-        }
-
-        // Save user
-        $entityManager->persist($user);
-        $entityManager->flush();
-
-        return new JsonResponse(['message' => 'User registered successfully'], JsonResponse::HTTP_CREATED);
+        return new JsonResponse($result, isset($result['error']) ? JsonResponse::HTTP_BAD_REQUEST : JsonResponse::HTTP_CREATED);
     }
 }
